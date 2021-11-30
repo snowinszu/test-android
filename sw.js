b5babbdb14e49764ef2c751b7fe52a6e96d3759b
snowinszu <86755838@qq.com>
2021-11-30 21:23:39 +0800

    update readme

--- sw.js
+++ sw.js
@@ -1,14 +1,6 @@
-self.importScripts('./dist/hls-proxy.js');
-// self.importScripts('../../opensource/sw/dist/peer-worker.min.js');
-// self.importScripts('https://cdn.jsdelivr.net/npm/swarmcloud-sw@latest/dist/peer-worker.min.js');
-
-// self.importScripts('https://cdn.jsdelivr.net/npm/swarmcloud-sw@latest/dist/sw.js')
-
-const proxy = new HlsProxy({
-    // logLevel: 'debug',
-});
-
+// self.importScripts('./dist/hls-proxy.js');
+self.importScripts('https://cdn.jsdelivr.net/npm/swarmcloud-hls-sw@latest/dist/hls-proxy.js')
 
 
 console.warn(`HlsProxy version ${HlsProxy.version}`);
-proxy.register();
+
